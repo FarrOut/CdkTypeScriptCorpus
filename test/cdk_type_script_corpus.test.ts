463c3b50c394956c47908582441b0fd3d2bdbad1
Cyber Captain <cybercaptain@duck.com>
2024-04-08 14:11:26 +0200

    Initial commit

---
+++ test/cdk_type_script_corpus.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkTypeScriptCorpus from '../lib/cdk_type_script_corpus-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk_type_script_corpus-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkTypeScriptCorpus.CdkTypeScriptCorpusStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
